81fa8b0e9d34d46a22630e5a5a5783c0ae535d30
javialroro <102406440+javialroro@users.noreply.github.com>
2023-06-02 20:33:26 -0600

    cambiosxd

--- Age of Pirates/src/Entidad.java
+++ Age of Pirates/src/Entidad.java
@@ -1,2 +1,4 @@
 public class Entidad {
+    int x;
+    int y;
 }
--- Age of Pirates/src/Main.java
+++ Age of Pirates/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
-    Entidad [][] mapa = new Entidad[20][20];
+    Entidad [][] matriz1 = new Entidad[20][20];
+    Entidad [][] matriz2 = new Entidad[20][20];
+    Entidad [][] matriz3 = new Entidad[20][20];
+    Entidad [][] matriz4 = new Entidad[20][20];
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
