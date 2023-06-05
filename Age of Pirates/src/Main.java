064114c6e80ef9b97bb578034d932c506962f9d8
javialroro <102406440+javialroro@users.noreply.github.com>
2023-06-04 18:53:20 -0600

    Interfaz init

--- Age of Pirates/src/Main.java
+++ Age of Pirates/src/Main.java
@@ -1,9 +1,107 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
-    Entidad [][] matriz1 = new Entidad[20][20];
-    Entidad [][] matriz2 = new Entidad[20][20];
-    Entidad [][] matriz3 = new Entidad[20][20];
-    Entidad [][] matriz4 = new Entidad[20][20];
+    Entidad[][] matriz1 = new Entidad[20][20];
+    Entidad[][] matriz2 = new Entidad[20][20];
+    Entidad[][] matriz3 = new Entidad[20][20];
+    Entidad[][] matriz4 = new Entidad[20][20];
+
+    JButton[][] matrizBotones = new JButton[20][20];
+
+    JFrame frame = new JFrame("Age of Pirates");
+
+    JPanel panelIzq = new JPanel();
+    JPanel panelDer = new JPanel();
+
+    JButton p1 = new JButton("P1");
+    JButton p2 = new JButton("P2");
+    JButton p3 = new JButton("P3");
+    JButton p4 = new JButton("P4");
+
+    JButton siguienteTurno = new JButton("Siguiente Turno");
+
+    JTextArea chat = new JTextArea();
+
+    public Main() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(850, 850);
+        frame.setLayout(new GridBagLayout());
+
+        Dimension minSize = new Dimension(100, 100);
+        panelIzq.setLayout(new GridLayout(21, 21, 1, 1));
+        panelIzq.setMinimumSize(minSize);
+        panelIzq.setPreferredSize(minSize);
+        panelDer.setLayout(new GridLayout(21, 21, 1, 1));
+        panelDer.setMinimumSize(minSize);
+        panelDer.setPreferredSize(minSize);
+
+        dibujarMatriz(matriz1, panelIzq);
+        dibujarMatriz(matriz2, panelDer);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 0.4; // Ajusta el peso para reducir el tamaño
+        gbc.weighty = 1.0;
+        gbc.fill = GridBagConstraints.BOTH;
+
+        frame.add(panelIzq, gbc);
+
+        gbc.gridx = 1;
+        gbc.weightx = 0.4; // Ajusta el peso para reducir el tamaño
+        frame.add(panelDer, gbc);
+
+
+        chat.setEditable(false);
+        chat.setWrapStyleWord(true);
+        chat.setText("Chat");
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        //gbc.weightx = 0.4; // Ajusta el peso para reducir el tamaño
+        gbc.weighty = 0.2;
+        gbc.fill = GridBagConstraints.BOTH;
+        frame.add(chat, gbc);
+
+        JPanel botonesJugadores = new JPanel();
+        botonesJugadores.setLayout(new GridBagLayout());
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        botonesJugadores.add(p1, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        botonesJugadores.add(p2,gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        botonesJugadores.add(p3, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        botonesJugadores.add(p4, gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+        //gbc.weightx = 0.2;
+        frame.add(botonesJugadores, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.fill = GridBagConstraints.BOTH;
+        frame.add(siguienteTurno, gbc);
+
+        //frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void dibujarMatriz(Entidad[][] matriz, JPanel panelMatriz) {
+        for (int r = 0; r < 20; r++) {
+            for (int c = 0; c < 20; c++) {
+                matrizBotones[r][c] = new JButton();
+                panelMatriz.add(matrizBotones[r][c]);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Main main = new Main();
     }
 }
